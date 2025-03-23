267b5e573e70717c9fed2ab6bc759eb9a3238743
Tan Shi Wei Cody <72619037+A-Cup-Of-Coffee-Cody@users.noreply.github.com>
2025-03-23 11:26:29 +0800

    Removed Parsing

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello DotDashAcademy!");
+  res.send("DotDashAcademy Backend is Functional!");
 });
 
 // Route registration

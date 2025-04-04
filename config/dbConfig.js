bcca6228cfa618bd9cd69fe81265f7fc0d292883
Tan Shi Wei Cody <72619037+A-Cup-Of-Coffee-Cody@users.noreply.github.com>
2025-04-04 19:29:35 +0800

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,23 +1,27 @@
 require("dotenv").config(); // Import dotenv to read from the .env file
 
-console.log("DB Config:", {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD ? "********" : "Not Set",
-  server: process.env.DB_SERVER,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-  encrypt: process.env.DB_ENCRYPT,
-  trustServerCertificate: process.env.DB_TRUST_SERVER_CERTIFICATE,
-});
+// module.exports = {
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   server: process.env.DB_SERVER,
+//   database: process.env.DB_DATABASE,
+//   options: {
+//     encrypt: process.env.DB_ENCRYPT === "true", // Convert to boolean
+//     trustServerCertificate: process.env.DB_TRUST_SERVER_CERTIFICATE === "true", // Convert to boolean
+//     port: parseInt(process.env.DB_PORT, 10), // Ensure it's a number
+//   },
+// };
+
+require("dotenv").config(); // Import dotenv to read from the .env file
 
 module.exports = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_DATABASE,
+  user: "HQ4038",
+  password: "t+8wRe*1D*=6Th5?2xlc",
+  server: "dotdashacademy-database.database.windows.net",
+  database: "dotdashacademyDatabase",
+  port: 1433, // Ensure it's a number
   options: {
-    encrypt: process.env.DB_ENCRYPT === "true", // Convert to boolean
-    trustServerCertificate: process.env.DB_TRUST_SERVER_CERTIFICATE === "true", // Convert to boolean
-    port: parseInt(process.env.DB_PORT, 10), // Ensure it's a number
+    encrypt: true, // Convert to boolean
+    trustServerCertificate: false, // Convert to boolean
   },
 };

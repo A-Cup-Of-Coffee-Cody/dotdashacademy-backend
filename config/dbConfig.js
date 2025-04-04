ce858ad67d277dbab4220da21ea607720aa60526
Tan Shi Wei Cody <72619037+A-Cup-Of-Coffee-Cody@users.noreply.github.com>
2025-04-04 19:23:29 +0800

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,15 @@
 require("dotenv").config(); // Import dotenv to read from the .env file
 
+console.log("DB Config:", {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD ? "********" : "Not Set",
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  encrypt: process.env.DB_ENCRYPT,
+  trustServerCertificate: process.env.DB_TRUST_SERVER_CERTIFICATE,
+});
+
 module.exports = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
